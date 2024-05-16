6b6796ff41df5c8e13bb3e944c27a208d3a23bde
Angela Chen <achen313@calpoly.edu>
2024-05-15 20:17:07 -0700

    change capactiy plan to 0

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -51,11 +51,11 @@ def get_capacity_plan():
     Start with 1 capacity for 50 potions and 1 capacity for 10000 ml of potion. Each additional 
     capacity unit costs 1000 gold.
     """
-    buy = 1
-    print("bought potion capactiy")
+    buy = 0
+    print("bought 0 potion capactiy")
 
     return {
-        "potion_capacity": buy,
+        "potion_capacity": 0,
         "ml_capacity": 0
         }
 
